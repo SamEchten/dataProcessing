73f0aebbac0fefbeed42f154e5351ce9fee1f86d
SamEchten <SamEchten@users.noreply.github.com>
2022-03-25 13:34:32 +0100

    Error message updated

--- api/public/js/apiHelper.js
+++ api/public/js/apiHelper.js
@@ -2,62 +2,67 @@ const serverInfo = {
     serverIp: "http://localhost:3030"
 }
 
+function fireError(err) {
+    Swal.fire({
+        title: 'Oops!',
+        text: err,
+        icon: 'error',
+        confirmButtonText: 'Continue'
+    })
+}
+
 const getAllCountries = async() => {
     try {
         let response = await fetch(serverInfo.serverIp + "/country/getAll/");
-        let json = await response.json();
-        return json;
+        if(response.status == 200) {
+            let json = await response.json();
+            return json;
+        } else {
+            fireError("Er is geen data gevonden voor dit land");
+        }
     } catch(err) {
-        Swal.fire({
-            title: 'Error!',
-            text: err,
-            icon: 'error',
-            confirmButtonText: 'Cool'
-        })
+        fireError(err);
     }
 }
 
 const getGniMaleByCountry = async(id) => {
     try {
         let response = await fetch(serverInfo.serverIp + "/gniMale/byCountryId/"+id+"");
-        let json = await response.json();
-        return json;
+        if(response.status == 200) {
+            let json = await response.json();
+            return json;
+        } else {
+            fireError("Er is geen data gevonden voor dit land");
+        }
     } catch(err) {
-        Swal.fire({
-            title: 'Error!',
-            text: err,
-            icon: 'error',
-            confirmButtonText: 'Cool'
-        })
+        fireError(err);
     }
 }
 
 const getGniFemaleByCountry = async(id) => {
     try {
         let response = await fetch(serverInfo.serverIp + "/gniFemale/byCountryId/"+id+"");
-        let json = await response.json();
-        return json;
+        if(response.status == 200) {
+            let json = await response.json();
+            return json;
+        } else {
+            fireError("Er is geen data gevonden voor dit land");
+        }
     } catch(err) {
-        Swal.fire({
-            title: 'Error!',
-            text: err,
-            icon: 'error',
-            confirmButtonText: 'Cool'
-        })
+        fireError(err);
     }
 }
 
 const getGdpByCountry = async(id) => {
     try {
         let response = await fetch(serverInfo.serverIp + "/gdp/byCountryId/"+id+"");
-        let json = await response.json();
-        return json;
+        if(response.status == 200) {
+            let json = await response.json();
+            return json;
+        } else {
+            fireError("Er is geen data gevonden voor dit land");
+        }
     } catch(err) {
-        Swal.fire({
-            title: 'Error!',
-            text: err,
-            icon: 'error',
-            confirmButtonText: 'Cool'
-        })
+        fireError(err);
     }
 }
